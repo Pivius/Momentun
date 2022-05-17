c3e187d0db959396264f95274a45e0993de8b630
Pivius <pivisens@gmail.com>
2022-05-17 17:18:42 +0200

    Player can now step while jumping over surfaces like stairs

--- code/controller/momentum_controller.cs
+++ code/controller/momentum_controller.cs
@@ -11,16 +11,17 @@ namespace Momentum
 		{
 			var velocity = Velocity;
 			AirAccelerate.Move( ref velocity,
-					  WishVelocity,
-					  (float)MoveProp["MaxSpeed"],
-					  (float)MoveProp["SideStrafeMaxSpeed"],
-					  (float)MoveProp["AirAccelerate"],
-					  (float)MoveProp["StrafeAcceleration"],
-					  (float)MoveProp["AirStopAcceleration"],
-					  (float)MoveProp["AirControl"] );
+						WishVelocity,
+						(float)MoveProp["MaxSpeed"],
+						(float)MoveProp["SideStrafeMaxSpeed"],
+						(float)MoveProp["AirAccelerate"],
+						(float)MoveProp["StrafeAcceleration"],
+						(float)MoveProp["AirStopAcceleration"],
+						(float)MoveProp["AirControl"] );
 			Velocity = velocity;
 			Velocity += BaseVelocity;
-			TryPlayerMove();
+			//TryPlayerMove();
+			StepMove();
 			Velocity -= BaseVelocity;
 		}
 
